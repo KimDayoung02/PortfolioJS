77b115ef7887580ffe95e6ec8d599979784f2d92
KimDayoung02 <dayoung7833@gmail.com>
2022-10-31 18:29:45 +0900

    포트폴리오 : 슬라이드 적용 및 스타일링
    프로젝트 부분에 슬라이드 적용 및 스타일링을 완료

--- portfolio.js
+++ portfolio.js
@@ -20,3 +20,21 @@ anime
     easing: "easeOutExpo",
     delay: 10000,
   });
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
